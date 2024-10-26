6c829546f916139306498fd88cb0ebe6028c1c1a
buwanikaveesha <buwanikaveesha@gmail.com>
2024-10-26 14:19:13 +0530

    create home/login and signup pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/HomePage/Home';
+import Login from './pages/LoginPage/Login';
+import SignUp from './pages/SignUpPage/SignUp';
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
