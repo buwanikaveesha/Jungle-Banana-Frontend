43e1fb0fbacd6c9b29e383f31172b8c52ad32ffb
buwanikaveesha <buwanikaveesha@gmail.com>
2024-10-26 15:04:50 +0530

    connect to the backend sever and create signup function

--- src/pages/SignUpPage/SignUp.jsx
+++ src/pages/SignUpPage/SignUp.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './SignUp.css';
 
 const SignUp = () => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:3000/api/signup", {
+      username,
+      email,
+      password
+    })
+    .then(res => {
+      if(res.data.status){
+        navigate('/login');
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    })
+
+  }
+
   return (
-    <div>SignUp</div>
+    <div className="signup-form-container">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" onChange={(e) => setUsername(e.target.value)}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email"onChange={(e) => setEmail(e.target.value)}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)}/>
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
   );
 }
 
