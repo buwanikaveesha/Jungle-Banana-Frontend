684b094df6a2e020190101295f5f552a94a2e1b5
buwanikaveesha <buwanikaveesha@gmail.com>
2024-10-31 12:56:59 +0530

    added the memory-card image to the minigame page

--- src/pages/MiniGamesPage/MiniGames.jsx
+++ src/pages/MiniGamesPage/MiniGames.jsx
@@ -5,34 +5,33 @@ import './MiniGames.css';
 const MiniGames = () => {
   return (
     <div><SlideMenuBar /> {/* Add SlideMenuBar here */}
-    <div className="mini-games-container">
-      <h1 className="title">MINI GAMES</h1>
-      <div className="games">
-        {/* Candy Crush Section */}
-        <div className="game-card">
-          <h2>Candy Crush</h2>
-          <br></br>
-          <div className="game-preview candy-crush">
-            <div className="candy-background" />
-            <button className="start-button2">Start</button>
-          </div>
-        </div>
-
-        {/* Memory Card Section */}
-        <div className="game-card">
-          <h2>Memory Card</h2>
-          <br></br>
-          <div className="game-preview memory-card">
-            <div className="memory-grid">
-              {Array.from({ length: 16 }).map((_, index) => (
-                <div key={index} className="memory-item"></div>
-              ))}
+      <div className="mini-games-container">
+        <h1 className="title">MINI GAMES</h1>
+        <div className="games">
+          {/* Candy Crush Section */}
+          <div className="game-card">
+            <h2>Candy Crush</h2>
+            <br></br>
+            <div className="game-preview candy-crush">
+              <div className="candy-background" />
+              <button className="start-button2">Start</button>
             </div>
-            <button className="start-button2">Start</button>
+          </div>
+
+          {/* Memory Card Section */}
+          <div className="game-card">
+            <h2>Memory Card</h2>
+            <br></br>
+            <div className="game-preview memory-card">
+              <div className="memoryCard-background">
+                
+              </div>
+                <button className="start-button2">Start</button>
+              
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
