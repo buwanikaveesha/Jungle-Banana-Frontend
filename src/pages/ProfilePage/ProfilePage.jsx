c26e8122d96c45329a5ad134c9b0fb031341416a
buwanikaveesha <buwanikaveesha@gmail.com>
2024-11-07 12:32:22 +0530

    create profile page and load user info

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,31 +1,69 @@
-import React from 'react';
-import userPic from '../../assets/images/user pic.jpg';
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import ProfilePic from '../../assets/images/user pic.jpg';
 import SlideMenuBar from '../../components/NavBar/NavBar';
+import AuthContext from '../../context/AuthContext';
 import './ProfilePage.css';
 
 const ProfilePage = () => {
+  const { token } = useContext(AuthContext);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/user/profile', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setUser(response.data);
+      } catch (error) {
+        console.error('Error fetching profile:', error);
+      }
+    };
+
+    if (token) {
+      fetchProfile();
+    }
+  }, [token]);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <SlideMenuBar /> {/* Add SlideMenuBar here */}
-      <div className="profile-page">
-        <h1 className="profile-title">PROFILE</h1>
-        <br></br>
-        <div className="profile-container">
-          <div className="profile-picture">
-            <img
-              src={userPic}
-              alt="Profile"
-            />
+      <SlideMenuBar />
+      <div className="profile-title">
+          <h1>PROFILE</h1>
+        </div>
+      <div className="profile-container">
+        
+        <div className='profile-card'>
+          <div className="profile-image">
+            <img src={ProfilePic} alt="Profile" />
           </div>
-          <div className="profile-details">
-            <p>Name: Buwani</p>
-            <p>Email: buwani@gmail.com</p>
-            <p>Current Score: 100</p>
-            <p>Current Position: 1st</p>
+          <div className="profile-info">
+            <p><strong>Username:</strong> {user.username}</p>
+            <p><strong>Email:</strong> {user.email}</p>
+            <p><strong>Rank:</strong></p>
+            <div className='rank-difficulty'>
+              <p>Easy: {user.rank.easy}</p>
+              <p>Medium: {user.rank.medium}</p>
+              <p>Hard: {user.rank.hard}</p>
+            </div>
+            <p><strong>Scores:</strong></p>
+            <div className='score-difficulty'>
+              <p>Easy: {user.score.easy}</p>
+              <p>Medium: {user.score.medium}</p>
+              <p>Hard: {user.score.hard}</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
+
   );
 };
 
